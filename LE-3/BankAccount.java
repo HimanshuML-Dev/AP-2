b5de72f1538ecdd0ccfa67ece44d3415dd59cbb7
Himanshu Nagar <himanshunagar807@gmail.com>
2026-09-06 00:09:42 +0530

    commit

---
+++ LE-3/BankAccount.java
@@ -0,0 +1,16 @@
+abstract class BankAccount {
+
+    private String accNo;
+    protected double balance;
+
+    public BankAccount(String accNo, double balance) {
+        this.accNo = accNo;
+        this.balance = balance;
+    }
+
+    public abstract void withdraw(double amt);
+
+    public double getBalance() {
+        return balance;
+    }
+}
